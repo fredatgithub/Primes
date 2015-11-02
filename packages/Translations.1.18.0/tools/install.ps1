7691261fb43800e26ba803da2287a00b57e9c3e6
Freddy Juhel <domaine75@gmail.com>
2015-11-02 20:35:58 +0100

    add primes files

---
+++ packages/Translations.1.18.0/tools/install.ps1
@@ -0,0 +1,13 @@
+param($installPath, $toolsPath, $package, $project)
+// search for <Content Include="Translations.xml">
+// and add if non existant
+// <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+// in order to have this:
+/*
+<ItemGroup>
+    <Content Include="Translations.xml">
+      <CopyToOutputDirectory>Always</CopyToOutputDirectory>
+    </Content>
+  </ItemGroup>
+*/
+// in csproj file
