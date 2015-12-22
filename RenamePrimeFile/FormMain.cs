f0955fc4080fd22cfa93c0bfa73c9beab2e586e0
fred <domaine75@gmail.com>
2015-12-23 00:48:27 +0100

    Update FormMain.cs

--- RenamePrimeFile/FormMain.cs
+++ RenamePrimeFile/FormMain.cs
@@ -579,7 +579,7 @@ namespace RenamePrimeFile
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
