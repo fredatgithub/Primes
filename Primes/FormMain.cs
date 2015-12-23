5b8e73721c418ca1e43ea74c429c34f7575bcf7d
fred <domaine75@gmail.com>
2015-12-24 00:19:58 +0100

    Update FormMain.cs

--- Primes/FormMain.cs
+++ Primes/FormMain.cs
@@ -538,7 +538,7 @@ namespace Primes
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
